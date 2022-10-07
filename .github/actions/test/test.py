24fff87e707cfd91363d09281af431f8c6965f21
Steven Silvester <steven.silvester@ieee.org>
2022-10-07 06:54:30 -0500

    test

---
+++ .github/actions/test/test.py
@@ -0,0 +1,8 @@
+import os
+
+from ghapi.core import GhApi
+
+gh = GhApi("blink1073", "test-python-project")
+user = os.environ['ACTOR']
+collab_level = gh.repos.get_collaborator_permission_level(user)
+print(collab_level["permission"] == "admin")
